84d79bc982a9a558554db662a313b50bbe5b67ad
Augusto Godoy <augustog5896@gmail.com>
2025-01-20 20:18:56 -0300

    add base card project

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,7 +1,6 @@
 defmodule Cards do
   @moduledoc """
   Documentation for `Cards`.
-  Provides methods for creating, shuffling, and checking the contents of a deck of cards.
   """
 
   @doc """
@@ -14,29 +13,6 @@ defmodule Cards do
 
   """
   def hello do
-    "hi there!"
     :world
   end
-
-  def create_deck do
-   values = ["ace","two","three","four","five","six","seven","eight","nine","ten","jack","queen","king"]
-   suits = ["Spades","Clubs","hearts","Diamonds"]
-
-   for suit <- suits, value <- values, do: "#{value} of #{suit}"
-
-  end
-
-
-  def shuffle(deck) do
-    Enum.shuffle(deck)
-  end
-
-  def contains?(deck, card) do
-    Enum.member?(deck, card)
-  end
-
-
-
- end
-
-
+end
