a1f4a63a3d9aa0f62285e6d797e37f2a7223c898
Augusto Godoy <augustog5896@gmail.com>
2025-01-22 17:55:33 -0300

    cart game

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -12,7 +12,39 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["ace", "two", "three"]
+    suits = ["spades", "clubs", "hearts"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    {split, _splitThrow} = Enum.split(deck, hand_size)
+    split
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "error"
+    end
   end
 end
